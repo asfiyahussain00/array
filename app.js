ae36566c7a57dd01e2a4ca993bdf1352adc00155
Asfiya hussain <126233785+asfiyahussain00@users.noreply.github.com>
2024-10-18 16:46:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+// array
+// let arry =[1,2,3,4,5,6,7,8,9,10];
+// console.log(arry[0]);
+
+
+
+//  let student_data =["Asfiya" , "hussain" , "female" ,1234  ]
+// //  console.log(student_data[0] , student_data[3]);
+//  student_data.push("web and app development");
+// // console.log(student_data)
+// // console.log(student_data[4]);
+// // console.log(student_data.length);
+
+
+
+
+// let age =[]
+// age[0]=23;
+// age[1]=26;
+// age[2]=27;
+// age[3]=30
+// console.log(age[2]);
+
+
+//lenght to know array value
+// let num =[1,2,3,4,5,6];
+// document.write(num.length);
+
+//push : add value in last of array
+// let num =[1,2,3,4,5];
+// (num.push(6,7));
+// document.write(num);
+
+
+
+//pop : to remove last  value
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.pop();
+// console.log(fruits);
+
+
+
+// let student_data =["Asfiya" , "hussain" , "female" ,1234  ];
+// (student_data.pop());
+// console.log(student_data);
+
+
+//shift Remove first  value in array
+// let num =[1 , 2 , 3 , 4 , 5];
+// num.shift();
+// console.log(num);
+
+
+//unshift add value in start of array
+// let num =[2,3,4,5,6,];
+// num.unshift(1);
+// console.log(num)
+
+
+
+
+
+
+
